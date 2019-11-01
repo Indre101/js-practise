71a72a05845c8dc8038fc59b6cb7b5beb0867027
indre101 <zygaityte.indre@gmail.com>
2019-11-01 10:08:03 +0100

    added variables

---
+++ variables/script.js
@@ -0,0 +1,29 @@
+const arrOfObjects = [
+  {
+    id: 1,
+    name: "Jonas",
+    lastName: "Doe",
+    hobbies: ["fishing", "sowing", "driving"],
+    isAvailable: true
+  },
+  {
+    id: 2,
+    name: "Jonas",
+    lastName: "Doe",
+    hobbies: ["fishing", "sowing", "driving"],
+    isAvailable: true
+  },
+  {
+    id: 3,
+    name: "Jonas",
+    lastName: "Doe",
+    hobbies: ["fishing", "sowing", "driving"],
+    isAvailable: false
+  }
+];
+
+const hobbies = arrOfObjects.filter(available => {
+  return available.isAvailable === true;
+});
+
+console.log(hobbies);
